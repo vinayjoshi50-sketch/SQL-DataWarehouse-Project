fbfe8452e3c03849a8772a9f3ec06dee038bc184
vinayjoshi50-sketch <vinayjoshi50@gmail.com>
2025-09-21 00:27:03 +0530

    Update ddl_bronze.sql

--- Scripts/Bronze/ddl_bronze.sql
+++ Scripts/Bronze/ddl_bronze.sql
@@ -12,6 +12,7 @@ Script Purpose:
 --Customer table
 if object_id('bronze.crm_cust_info','U') is not null
 	drop table bronze.crm_cust_info;
+GO
 create table bronze.crm_cust_info (
 	cst_id int,
 	cst_key nvarchar(50),
@@ -21,10 +22,12 @@ create table bronze.crm_cust_info (
 	cst_gndr nvarchar(50),
 	cst_create_date date
 );
+GO
 
 --Product Table
 if object_id('bronze.crm_prd_info','U') is not null
 	drop table bronze.crm_prd_info;
+GO
 create table bronze.crm_prd_info (
 	prd_id int,
 	prd_key nvarchar(50),
@@ -34,10 +37,12 @@ create table bronze.crm_prd_info (
 	prd_start_dt datetime,	
 	prd_end_dt datetime
 ) ;
+GO
 
 --Sales Details
 if object_id('bronze.crm_sales_details','U') is not null
 	drop table bronze.crm_sales_details;
+GO
 create table bronze.crm_sales_details(
 	sls_ord_num nvarchar(50),
 	sls_prd_key nvarchar(50),
@@ -49,31 +54,40 @@ create table bronze.crm_sales_details(
 	sls_quantity int,
 	sls_price int
 );
-
+GO
+	
 --ERP Tables
 --Table-1
 if object_id('bronze.erp_loc_a101','U') is not null
 	drop table bronze.erp_loc_a101;
+GO
 create table bronze.erp_loc_a101(
 	cid nvarchar(50),
 	cntry nvarchar(50)
 ) ;
+GO
 
 --Table-2
 if object_id('bronze.erp_cust_az12','U') is not null
 	drop table bronze.erp_cust_az12;
+GO
 create table bronze.erp_cust_az12(
 	cid nvarchar(50),
 	bdate date,
 	gen nvarchar(50)
 );
+GO
 
 --Table-1
 if object_id('bronze.erp_px_cat_g1v2','U') is not null
 	drop table bronze.erp_px_cat_g1v2;
+GO
 create table bronze.erp_px_cat_g1v2(
 	id  nvarchar(50),
 	cat  nvarchar(50),
 	subcat  nvarchar(50),
 	maintenance nvarchar(50)
 );
+GO
+
+GO
